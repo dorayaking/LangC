905538057ea157a1074e43751866b9e143dbf34e
Ryosuke Inoue <d2531003@eng.kitakyu-u.ac.jp>
2023-11-17 13:58:29 +0900

    eescape

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(void){
+    int i;
     printf("Hello World!!\n");
     printf("Hello Ryosuke!!\n");
+    printf("I'm Ryosuke\n");
+    printf("Fukuoka prefectere\n");
+    for(i=1;i<11;i++){
+        printf("%d\n", i);
+    }
     return 0;
 }
